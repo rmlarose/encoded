d35dd348fe3c559b0f7f933a8f3c0bac9131743a
YanisLeFur <64954297+YanisLeFur@users.noreply.github.com>
2025-02-18 18:08:21 +0100

    Deutsch-Jozsa implementation

---
+++ encoded/dj.py
@@ -0,0 +1,42 @@
+import cirq
+import qsimcirq
+import numpy as np
+from typing import Sequence, Dict
+
+
+def dj(qreg: Sequence[cirq.Qid]) -> cirq.Circuit:
+    """
+    Args:
+        qregs: set of N+1 qubits where the N first qubits represent the reqister for querying the oracle and the N+1-th qubit is the register for storing the answer of the oracle
+    Returns:
+        cirq.Circuit: quantum circuit for the Deutsch Jozsa algorithm
+    """
+
+    # implementing quantum electro
+    circuit_dj = cirq.Circuit()
+    n = len(qreg) - 1
+
+    # define oracle with type = 0 (unbalanced) or 1 (balanced) and define value of oracle (for unbalanced case)
+    oracleType, oracleValue = np.random.randint(2), np.random.randint(2)
+    if oracleType == 1:
+        oracleValue = np.random.randint(1, 2**n)
+
+    # initialization
+    circuit_dj.append(cirq.H.on_each(qreg[:n]))
+    circuit_dj.append(cirq.X(qreg[n]))
+    circuit_dj.append(cirq.H(qreg[n]))
+
+    # Oracle
+    if oracleType == 0:  # If the oracleType is "0", the oracle returns oracleValue for all input.
+        if oracleValue == 1:
+            circuit_dj.append(cirq.X(qreg[n]))
+    else:  # Otherwise, it returns the inner product of the input with a (non-zero bitstring)
+        print(range(n))
+        for i in range(n):
+            if oracleValue & (1 << i):
+                circuit_dj.append(cirq.CNOT.on(qreg[n - i - 1], qreg[n]))
+
+    # finalization
+    circuit_dj.append(cirq.H.on_each(qreg[:n]))
+
+    return circuit_dj, oracleType, oracleValue
