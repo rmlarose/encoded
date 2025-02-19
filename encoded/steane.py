66b8fb2cdf4640637ab42e48b183ea3166eb3386
YanisLeFur <64954297+YanisLeFur@users.noreply.github.com>
2025-02-19 17:51:37 +0100

    Implementing steane basic operations

---
+++ encoded/steane.py
@@ -0,0 +1,43 @@
+import cirq
+from typing import Sequence
+
+
+def encoding_steane(qreg: Sequence[cirq.Qid]) -> cirq.Circuit:
+    circuit = cirq.Circuit()
+    circuit = cirq.Circuit()
+
+    circuit.append(cirq.H.on(qreg[0]))
+    circuit.append(cirq.H.on(qreg[4]))
+    circuit.append(cirq.H.on(qreg[6]))
+
+    circuit.append(cirq.CNOT.on(qreg[0], qreg[1]))
+    circuit.append(cirq.CNOT.on(qreg[4], qreg[5]))
+
+    circuit.append(cirq.CNOT.on(qreg[6], qreg[3]))
+    circuit.append(cirq.CNOT.on(qreg[6], qreg[5]))
+    circuit.append(cirq.CNOT.on(qreg[4], qreg[2]))
+
+    circuit.append(cirq.CNOT.on(qreg[0], qreg[3]))
+    circuit.append(cirq.CNOT.on(qreg[4], qreg[1]))
+    circuit.append(cirq.CNOT.on(qreg[3], qreg[2]))
+
+    return circuit
+
+
+def logical_H(qreg: Sequence[cirq.Qid]) -> cirq.Circuit:
+    circuit = cirq.Circuit()
+    circuit.append(cirq.H.on_each(qreg))
+    return circuit
+
+
+def logical_X(qreg: Sequence[cirq.Qid]) -> cirq.Circuit:
+    circuit = cirq.Circuit()
+    circuit.append(cirq.X.on_each(qreg))
+    return circuit
+
+
+def logical_CNOT(qreg: Sequence[cirq.Qid]) -> cirq.Circuit:
+    circuit = cirq.Circuit()
+    for i in range(7):
+        circuit.append(cirq.CNOT.on(qreg[i], qreg[i + 7]))
+    return circuit
