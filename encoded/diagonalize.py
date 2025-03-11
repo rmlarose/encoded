967100ec876543d629c14f783a3aea73fcc709a2
Ryan LaRose <rmlarose@msu.edu>
2025-03-11 16:57:56 -0400

    Checkpoint.

--- encoded/diagonalize.py
+++ encoded/diagonalize.py
@@ -58,12 +58,16 @@ def get_measurement_circuit(stabilizer_matrix):
 
         # Apply Hadamards to swap X and Z rows to transform X matrix to have rank nump
         if np.linalg.matrix_rank(candidate_matrix) == nump:
+            print("Row combination:", row_combination)
             for i, c in enumerate(row_combination):
                 if c == "Z":
                     z_matrix[i] = x_matrix[i]
                     measurement_circuit.append(cirq.H.on(qreg[i]))
             x_matrix = candidate_matrix
             break
+    
+    print("X matrix")
+    print(x_matrix)
     for j in range(nump):
         if x_matrix[j,j] == 0:
             i = j + 1
@@ -86,7 +90,88 @@ def get_measurement_circuit(stabilizer_matrix):
                 z_matrix[j] = (z_matrix[j] + z_matrix[i]) % 2
 
                 measurement_circuit.append(cirq.CNOT.on(qreg[j], qreg[i]))
-    
+
+    for j in range(nump-1, 0, -1):
+        for i in range(j):
+            if x_matrix[i, j] == 1:
+                x_matrix[i] = (x_matrix[i] + x_matrix[j]) % 2
+                z_matrix[j] = (z_matrix[j] + z_matrix[i]) % 2
+
+                measurement_circuit.append(cirq.CNOT.on(qreg[j], qreg[i]))
+
+    for i in range(nump):
+        if z_matrix[i,i] == 1:
+            z_matrix[i,i] = 0
+            measurement_circuit.append(cirq.S.on(qreg[i]))
+        
+        for j in range(i):
+            if z_matrix[i,j] == 1:
+                z_matrix[i,j] = 0
+                z_matrix[j,i] = 0
+                measurement_circuit.append(cirq.CZ.on(qreg[j], qreg[i]))
+
+    for i in range(nump):
+        row = x_matrix[i].copy()
+        x_matrix[i] = z_matrix[i]
+        z_matrix[i] = row
+
+        measurement_circuit.append(cirq.H.on(qreg[i]))
+
+    return measurement_circuit, np.concatenate((z_matrix, x_matrix))
+
+
+def get_measurement_circuit_triangular_color_code(stabilizer_matrix):
+    numq = len(stabilizer_matrix) // 2 # number of qubits
+    nump = len(stabilizer_matrix[0]) # number of paulis
+    z_matrix = stabilizer_matrix.copy()[:numq]
+    x_matrix = stabilizer_matrix.copy()[numq:]
+
+    measurement_circuit = cirq.Circuit()
+    qreg = cirq.LineQubit.range(numq)
+
+    # Find a combination of rows to make X matrix have rank nump
+    row_combination = "XZ" * (numq // 2) + "X"
+    print("Row combination:", row_combination)
+    assert len(row_combination) == numq
+    candidate_matrix = np.array([
+        z_matrix[i] if c=="Z" else x_matrix[i] for i, c in enumerate(row_combination)
+    ])
+
+    # Apply Hadamards to swap X and Z rows to transform X matrix to have rank nump
+    assert np.linalg.matrix_rank(candidate_matrix) == nump
+    print("Matrix is full rank")
+    for i, c in enumerate(row_combination):
+        if c == "Z":
+            z_matrix[i] = x_matrix[i]
+            measurement_circuit.append(cirq.H.on(qreg[i]))
+    x_matrix = candidate_matrix
+    print("X matrix")
+    print(x_matrix)
+    assert np.linalg.matrix_rank(x_matrix) == nump
+
+    for j in range(nump):
+        if x_matrix[j,j] == 0:
+            i = j + 1
+            while x_matrix[i,j] == 0:
+                i += 1
+
+            x_row = x_matrix[i].copy()
+            x_matrix[i] = x_matrix[j]
+            x_matrix[j] = x_row
+
+            z_row = z_matrix[i].copy()
+            z_matrix[i] = z_matrix[j]
+            z_matrix[j] = z_row
+
+            measurement_circuit.append(cirq.SWAP.on(qreg[j], qreg[i]))
+
+        for i in range(j + 1, numq):
+            if x_matrix[i,j] == 1:
+                x_matrix[i] = (x_matrix[i] + x_matrix[j]) % 2
+                z_matrix[j] = (z_matrix[j] + z_matrix[i]) % 2
+
+                measurement_circuit.append(cirq.CNOT.on(qreg[j], qreg[i]))
+
     for j in range(nump-1, 0, -1):
         for i in range(j):
             if x_matrix[i, j] == 1:
